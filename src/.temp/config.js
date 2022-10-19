2415ba16ed77da5211b356d9e76a78207f9970c0
precious <chris@barnesy.me>
2022-10-19 16:51:53 -0400

    fixes ignore

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Webtime",
-  "siteUrl": "https://barnesy.me/",
-  "version": "0.7.23",
-  "catchLinks": true
-}
