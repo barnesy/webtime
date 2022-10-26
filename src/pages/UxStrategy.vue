aec046f1010d76e09d88b991658bf010b8490d32
jamesjdistefano <jamesjdistefano@gmail.com>
2022-10-26 09:54:32 -0400

    Edits copy on UX Strat

--- src/pages/UxStrategy.vue
+++ src/pages/UxStrategy.vue
@@ -3,21 +3,21 @@
     <template #page-header>
       <Header class="v2">
         <template>Design for Scale</template>
-        <template #support>Designing the user experience requires us to be the glue between design and development. To have deep knowledge of the consequenses of our decisions.</template>
+        <template #support>Designing the user experience requires us to be the glue between design and development, and requires deep knowledge of the consequenses of our decisions.</template>
       </Header>
     </template>
 
     <PageSection padding="8rem 3rem 8rem" class="center">
         <template #title>System Design</template>
-        <p>We are creating something dynamic. The work must also be dynamic. We must have in mind how each decision cascades to create our final product and what effects our ability to scale. We can accomplish this through aligning on guiding principles and creating flexible systems.</p>
+        <p>We are creating something dynamic. The work must also be dynamic. We must have in mind how each decision cascades to create our final product and what affects our ability to scale. We can accomplish this through aligning on guiding principles and creating flexible systems.</p>
 
         <g-image src="/img/components.png" />
 
-        <p>We can scale this system to support a set of websites that all need to share common design, heirarchy of content, and goals for the user. This allows us to effect change accross those sites in a way that is extremely streamlined.</p>
+        <p>We can scale this system to support a set of websites that share a common design, a hierarchy of content, and goals for the user. This allows us to affect change accross sites in a way that is extremely streamlined.</p>
 
         <g-image src="/img/structure.png" />
 
-        <p>We can use this system to build contextual UIs that, while the development can be more complex, we gain an easier path ways for the user and simplify the design.</p>
+        <p>We can use this system to build contextual UIs. While the development can be more complex, we gain a simplified design and easier pathways for the user.</p>
 
         <g-image src="/img/contextual-ui.png" />
     </PageSection>
@@ -43,7 +43,7 @@
     <PageSection class="narrow center dark" bgColor="black">
       <template #title>Broad to Specific</template>
       <template>
-        <p>We organize information from broad to specific, from High priority to low priority. Google tells us that structured internet should start broad in directories of information that lead us into more dense(long form) destinations of content.</p>
+        <p>We organize information from broad to specific, from high priority to low priority. Google tells us that structured internet content should start broad in directories of information that lead us into more dense (long form) destinations of content.</p>
 
         <g-image src="/img/ux-flow.png" />
       </template>
